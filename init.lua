1c3aed4d607a12bd6d614e38d3c591ba2165d229
John Cambefort <john.cambefort@philips.com>
2024-05-20 12:43:41 -0400

    feat: add nvim-tree

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.cmd([[
 	Plug 'neoclide/coc.nvim', {'branch': 'release'}
 	Plug 'nvim-lua/plenary.nvim'
 	Plug 'ibhagwan/fzf-lua', {'branch': 'main'}
+	Plug 'nvim-tree/nvim-tree.lua'
 	Plug 'nvim-tree/nvim-web-devicons'
 	Plug 'dyng/ctrlsf.vim'
 	Plug 'tpope/vim-commentary'
@@ -21,6 +22,73 @@ vim.cmd([[
 
 	call plug#end()
 ]])
+
+-- Nvim tree -------------
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- custom keybinds
+
+vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
+
+local function my_on_attach(bufnr)
+	local api = require "nvim-tree.api"
+
+	local function opts(desc)
+	return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+
+	local function edit_or_open()
+	  local node = api.tree.get_node_under_cursor()
+
+	  if node.nodes ~= nil then
+	    -- expand or collapse folder
+	    api.node.open.edit()
+	  else
+	    -- open file
+	    api.node.open.edit()
+	    -- Close the tree if file was opened
+	    api.tree.close()
+	  end
+	end
+
+	-- open as vsplit on current node
+	local function vsplit_preview()
+	  local node = api.tree.get_node_under_cursor()
+
+	  if node.nodes ~= nil then
+	    -- expand or collapse folder
+	    api.node.open.edit()
+	  else
+	    -- open file as vsplit
+	    api.node.open.vertical()
+	  end
+
+	  -- Finally refocus on tree if it was lost
+	  api.tree.focus()
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- on_attach
+	vim.keymap.set("n", "l", edit_or_open,          opts("Edit Or Open"))
+	vim.keymap.set("n", "L", vsplit_preview,        opts("Vsplit Preview"))
+	vim.keymap.set("n", "h", api.tree.close,        opts("Close"))
+	vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+end
+
+-- setup with some options
+require("nvim-tree").setup({
+	on_attach = my_on_attach
+})
+--------------------
 
 -- Some servers have issues with backup files, see #649
 vim.opt.backup = false
