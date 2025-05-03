b6dccfa54b02611985944d630054b98066ba329a
John Cambefort <john.cambefort@philips.com>
2025-05-03 19:15:30 -0400

    feat: oil settings

--- init.lua
+++ init.lua
@@ -784,13 +784,69 @@ require("lazy").setup({
 		"stevearc/oil.nvim",
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {},
+		opts = {
+			default_file_explorer = true,
+			lsp_file_methods = {
+				-- Enable or disable LSP file operations
+				enabled = true,
+				-- Time to wait for LSP file operations to complete before skipping
+				timeout_ms = 1000,
+				-- Set to true to autosave buffers that are updated with LSP willRenameFiles
+				-- Set to "unmodified" to only save unmodified buffers
+				autosave_changes = false,
+			},
+			keymaps = {
+				["g?"] = { "actions.show_help", mode = "n" },
+				["<CR>"] = "actions.select",
+				["<C-s>"] = { "actions.select", opts = { vertical = true } },
+				["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+				["<C-t>"] = { "actions.select", opts = { tab = true } },
+				["<C-p>"] = "actions.preview",
+				["<C-c>"] = { "actions.close", mode = "n" },
+				["<C-l>"] = "actions.refresh",
+				["-"] = { "actions.parent", mode = "n" },
+				["_"] = { "actions.open_cwd", mode = "n" },
+				["`"] = { "actions.cd", mode = "n" },
+				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+				["gs"] = { "actions.change_sort", mode = "n" },
+				["gx"] = "actions.open_external",
+				["g."] = { "actions.toggle_hidden", mode = "n" },
+				["g\\"] = { "actions.toggle_trash", mode = "n" },
+			},
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+				-- This function defines what is considered a "hidden" file
+				is_hidden_file = function(name, bufnr)
+					local m = name:match("^%.")
+					return m ~= nil
+				end,
+				-- This function defines what will never be shown, even when `show_hidden` is set
+				is_always_hidden = function(name, bufnr)
+					return false
+				end,
+				-- Sort file names with numbers in a more intuitive order for humans.
+				-- Can be "fast", true, or false. "fast" will turn it off for large directories.
+				natural_order = "fast",
+				-- Sort file and directory names case insensitive
+				case_insensitive = false,
+				sort = {
+					-- sort order can be "asc" or "desc"
+					-- see :help oil-columns to see which columns are sortable
+					{ "type", "asc" },
+					{ "name", "asc" },
+				},
+				-- Customize the highlight group for the file name
+				highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+					return nil
+				end,
+			},
+		},
 		-- Optional dependencies
-		dependencies = { { "echasnovski/mini.icons", opts = {} }, { "nvim-tree/nvim-web-devicons" } },
-		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-		config = function()
-			require("oil").setup()
-		end,
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		lazy = false,
 	},
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
